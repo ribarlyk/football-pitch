321484f74ccaa707b517a3a41772208c8638aaec
Pavel Hristov <hristov.pavel@zohomail.eu>
2023-04-08 16:38:12 +0300

    data persist in ls

--- src/components/Pitch.jsx
+++ src/components/Pitch.jsx
@@ -3,19 +3,34 @@ import { useEffect, useState } from "react"
 import Animations from "./SkeletonPlayers"
 import uniqid from "uniqid"
 import ShirtButton from "./ShirtButton"
+import PaginationOutline from "./Pagination"
 
 export default function Pitch() {
-    const [players, setPlayers] = useState('')
-    const [loading, setLoading] = useState(false)
-    const [goalkeeper, setGoalkeeper] = useState([])
-    const [defender, setDefender] = useState([])
-    const [midfielder, setMidfielder] = useState([])
-    const [attacker, setAttacker] = useState([])
+    const [players, setPlayers] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [goalkeeper, setGoalkeeper] = useState([]);
+    const [defender, setDefender] = useState([]);
+    const [midfielder, setMidfielder] = useState([]);
+    const [attacker, setAttacker] = useState([]);
+    const [page, setPage] = useState(1);
+    const [budget, setBudget] = useState(450);
+    const [substitue, setSubstitue] = useState([]);
+    const [myTeam, setMyTeam] = useState('');
+    const [isTeamChoosen, setIsTeamChoosen] = useState(false);
+    const [localStorageTeam, setLocalStorageTeam] = useState('');
+    console.log(Boolean(myTeam))
+    console.log(myTeam)
 
     useEffect(() => {
-        fetch(' https://api-football-v1.p.rapidapi.com/v3/players?league=39&season=2022', {
+        const team = JSON.parse(localStorage.getItem('team'))
+        setLocalStorageTeam(team)
+    }, [])
+
+    useEffect(() => {
+
+        fetch(`https://api-football-v1.p.rapidapi.com/v3/players?league=39&season=2022&page=${page}`, {
             headers: {
-                'X-RapidAPI-Key': '',
+                'X-RapidAPI-Key': '9a511f7146mshe0fab5844669c1dp1c1c5fjsn55edffb40906',
                 'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
             }
         }).then(res => {
@@ -31,9 +46,7 @@ export default function Pitch() {
         }).catch(err => {
             setLoading(!loading)
         })
-
-
-    }, [])
+    }, [page]) // да се изкара в папкa service и да се ползва axios
 
 
 
@@ -67,91 +80,156 @@ export default function Pitch() {
     }
 
     const onPlayerClickHandler = (player) => {
+        let sumToBuy = player.player.age || 10
         if (player.statistics[0].games.position === 'Goalkeeper') {
             if (goalkeeper.length < 1) {
                 setGoalkeeper((prev) => [...prev, player])
+            } else {
+                setSubstitue((prev) => [...prev, player])
             }
         } else if (player.statistics[0].games.position === 'Defender') {
             if (defender.length < 4) {
                 setDefender((prev) => [...prev, player])
+            } else {
+                setSubstitue((prev) => [...prev, player])
+
             }
         } else if (player.statistics[0].games.position === 'Midfielder') {
             if (midfielder.length < 4) {
                 setMidfielder((prev) => [...prev, player])
+            } else {
+                setSubstitue((prev) => [...prev, player])
+
             }
         } else if (player.statistics[0].games.position === 'Attacker') {
             if (attacker.length < 2) {
                 setAttacker((prev) => [...prev, player])
+            } else {
+                setSubstitue((prev) => [...prev, player])
+
             }
+
         }
+        if (substitue.length === 4) {
+            setIsTeamChoosen(true)
+        }
+
+        if (budget - sumToBuy < 0) {
+            alert('not enought money')
+        } else {
+            setBudget((prev => Number(prev) - Number(sumToBuy)))
+        }
+
+
     }
+
+
+    const saveTeamHandler = () => {
+        setMyTeam([...goalkeeper, ...defender, ...midfielder, ...attacker, ...substitue]);
+    }
+
+    if (myTeam.length > 0) {
+        localStorage.setItem('team', JSON.stringify(myTeam));
+    }
+
 
     return (
         <>
+            <div className="budget-container">
+                <h2>{budget} Mil</h2>
+            </div>
             <div className="container">
+
                 <div className="pitch-container">
                     <div className="pitch-background" >
                         <div className="row-one">
                             <ShirtButton />
                             <ShirtButton />
-                            <ShirtButton position={goalkeeper[0]?.player?.name} jersey={goalkeeper[0] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
-                            <ShirtButton />
-                            <ShirtButton />
 
+                            {localStorageTeam ? <ShirtButton position={localStorageTeam[0]?.player?.name} jersey={localStorageTeam[0] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={goalkeeper[0]?.player?.name} jersey={goalkeeper[0] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
+                            <ShirtButton />
+                            <ShirtButton />
                         </div>
                         <div className="row-two">
                             <div className="defender">
-                                <ShirtButton className="defender" position={defender[0]?.player?.name} jersey={defender[0] ? "assets/spainwhite.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[1]?.player?.name} jersey={localStorageTeam[1] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={defender[0]?.player?.name} jersey={defender[0] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
+                            </div>
+                            <div className="defender">
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[2]?.player?.name} jersey={localStorageTeam[2] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={defender[1]?.player?.name} jersey={defender[1] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
 
 
                             </div>
                             <div className="defender">
-                                <ShirtButton className="defender" position={defender[1]?.player?.name} jersey={defender[1] ? "assets/spainwhite.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[3]?.player?.name} jersey={localStorageTeam[3] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={defender[2]?.player?.name} jersey={defender[2] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
 
                             </div>
                             <div className="defender">
-                                <ShirtButton className="defender" position={defender[2]?.player?.name} jersey={defender[2] ? "assets/spainwhite.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
-
-                            </div>
-                            <div className="defender">
-                                <ShirtButton className="defender" position={defender[3]?.player?.name} jersey={defender[3] ? "assets/spainwhite.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[4]?.player?.name} jersey={localStorageTeam[4] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={defender[3]?.player?.name} jersey={defender[3] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
 
                             </div>
                         </div>
                         <div className="row-three">
                             <div className="midfielder">
-                                <ShirtButton position={midfielder[1]?.player?.name} jersey={midfielder[1] ? "assets/spainwhite.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[5]?.player?.name} jersey={localStorageTeam[5] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={midfielder[0]?.player?.name} jersey={midfielder[0] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
+
                             </div>
                             <div className="midfielder">
-                                <ShirtButton position={midfielder[0]?.player?.name} jersey={midfielder[0] ? "assets/arsenal;.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[6]?.player?.name} jersey={localStorageTeam[6] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={midfielder[1]?.player?.name} jersey={midfielder[1] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
+
                             </div>
                             <div className="midfielder">
-                                <ShirtButton position={midfielder[2]?.player?.name} jersey={midfielder[2] ? "assets/mancity.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[7]?.player?.name} jersey={localStorageTeam[7] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={midfielder[2]?.player?.name} jersey={midfielder[2] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
+
                             </div>
                             <div className="midfielder">
-                                <ShirtButton position={midfielder[3]?.player?.name} jersey={midfielder[3] ? "assets/arsenal;.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[8]?.player?.name} jersey={localStorageTeam[8] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={midfielder[3]?.player?.name} jersey={midfielder[3] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
+
                             </div>
                         </div>
 
                         <div className="row-four">
                             <div className="striker">
-                                <ShirtButton position={attacker[0]?.player?.name} jersey={attacker[0] ? "assets/deutschland.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[9]?.player?.name} jersey={localStorageTeam[9] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={attacker[0]?.player?.name} jersey={attacker[0] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
                             </div>
                             <div className="striker">
-                                <ShirtButton position={attacker[1]?.player?.name} jersey={attacker[1] ? "assets/brasilyellow.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[10]?.player?.name} jersey={localStorageTeam[10] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={attacker[1]?.player?.name} jersey={attacker[1] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
                             </div>
                         </div>
 
-                    </div>
+                        <div className="row-five">
+                            <div className="midfielder">
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[11]?.player?.name} jersey={localStorageTeam[11] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={substitue[0]?.player?.name} jersey={substitue[0] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
 
-                </div>
+                            </div>
+                            <div className="midfielder">
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[12]?.player?.name} jersey={localStorageTeam[12] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={substitue[1]?.player?.name} jersey={substitue[1] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
+
+                            </div>
+                            <div className="midfielder">
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[13]?.player?.name} jersey={localStorageTeam[13] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={substitue[2]?.player?.name} jersey={substitue[2] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
+
+                            </div>
+                            <div className="midfielder">
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[14]?.player?.name} jersey={localStorageTeam[14] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={substitue[3]?.player?.name} jersey={substitue[3] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
+
+                            </div>
+                            <div className="midfielder">
+                                {localStorageTeam ? <ShirtButton position={localStorageTeam[15]?.player?.name} jersey={localStorageTeam[15] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} /> : <ShirtButton position={substitue[4]?.player?.name} jersey={substitue[4] ? "assets/long-sleeve -goalkeeper.png=z-0,0_f-webp" : "assets/no-player.png=z-0,0_f-webp"} />}
+
+                            </div>
+                        </div>
+                    </div>
+                    {isTeamChoosen ? <button onClick={() => { saveTeamHandler() }} className="save-team">Save Team</button> : null}
+                    {isTeamChoosen ? <button className="set-captain" >Set Captain</button> : null}
+                </div >
                 <div className="players-container">
                     {createPlayerSection("goalkeepers", 'GOALKEEPERS', "Goalkeeper")}
                     {createPlayerSection("defenders", 'DEFENDERS', "Defender")}
                     {createPlayerSection("midfielders", 'MIDFIELDERS', "Midfielder")}
                     {createPlayerSection("attackers", 'ATTACKERS', "Attacker")}
+                    <PaginationOutline setPage={setPage} page={page} setLoading={setLoading} loading={loading} />
                 </div>
-            </div>
+            </div >
 
         </>
 
